9b7fc0ec31d19c8f19fb7e1bc91f98f62297d86c
Bruel Nicolas <bruel.nicolas85@gmail.com>
2015-03-18 17:14:09 +0100

    Reworked the test to work :kissing_heart:

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,20 +1,30 @@
 'use strict';
 var jsobjectDiff = require('../');
+var fs = require('fs');
 var p = require('path');
 var t = require('chai').assert;
+var json = JSON.parse(fs.readFileSync('test.json', 'utf8'));
 
-describe('jsobject-diff', function () {
-  describe('compare(files) with two files', function () {
-    it('should return an error if the files are not matching', function (done) {
-      var files = ['en_US', 'fr_FR'].map(function (lang) {
+
+describe('jsobject-diff', function() {
+  describe('compare(files) with two files', function() {
+    it('should return an error if the files are not matching', function(done) {
+      var files = json.files.map(function(fileTab) {
         return {
-          filepath: p.resolve(__dirname, './fixtures', lang + '.js'),
-          extract: function (object) {
-            return object.i18n[lang];
+          filepath: fileTab[0],
+          objectpath: fileTab[1].split('.'),
+          extract: function(object) {
+            function findObject(object, path, i) {
+              if (i < path.length - 1) {
+                return findObject(object[path[i]], path, ++i);
+              }
+              return object[path[i]];
+            }
+            return findObject(object, this.objectpath, 0);
           }
         };
       });
-      t.deepEqual(jsobjectDiff.compare(files), ["SEARCH_TEMPLATE_INTRO", "SEARCH_SOURCES_RESERVED_INTRO"]);
+      t.deepEqual(jsobjectDiff.compare(json.pre, files, json.post), ["SEARCH_TEMPLATE_INTRO", "SEARCH_SOURCES_RESERVED_INTRO"]);
       done();
     });
   });
