9724d3acd84f363b589678275e0147f61ef81b15
FG Ribreau <github@fgribreau.com>
2015-04-01 20:09:00 +0200

    test(jsobject): bring back tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 var _ = require('lodash');
-var objectPath = require('object-path');
+var objectPathTraverser = require('object-path');
 var assert = require('assert');
 
 module.exports = {
@@ -12,53 +12,54 @@ module.exports = {
     var filesObjects = files.map(function (file) {
       assert.strictEqual(typeof file.objectPath, 'string');
       var fileContent = pre + file.content + post;
-      return objectPath.get(eval(fileContent), file.objectPath);
+      // @todo use a try/catch
+      return objectPathTraverser.get(eval(fileContent), file.objectPath);
     });
 
-    var filesKeys = filesObjects.map(function(obj, index) {
+    var filesKeys = filesObjects.map(function (obj, index) {
       return {
         filePath: files[index].filePath,
         objectPath: files[index].objectPath,
         fileKeys: Object.keys(obj)
-      }
+      };
     });
 
-    return filesKeys.map(function(obj) {
+    return filesKeys.map(function (obj) {
       return {
         filePath: obj.filePath,
         objectPath: obj.objectPath,
-        comparedTo: _.flatten(filesKeys.map(function(otherObj){
-          if(obj.filePath == otherObj.filePath && obj.objectPath == otherObj.objectPath){
-            return []
+        comparedTo: _.flatten(filesKeys.map(function (otherObj) {
+          if (obj.filePath === otherObj.filePath && obj.objectPath === otherObj.objectPath) {
+            return [];
           }
           return {
             filePath: otherObj.filePath,
             objectPath: otherObj.objectPath,
-            inPlus: _.flatten(_.difference(obj.fileKeys, otherObj.fileKeys).map(function(differenceKeys){
-              return _.flatten(obj.fileKeys.map(function(objKey, index){
-                if(objKey == differenceKeys){
+            inPlus: _.flatten(_.difference(obj.fileKeys, otherObj.fileKeys).map(function (differenceKeys) {
+              return _.flatten(obj.fileKeys.map(function (objKey, index) {
+                if (objKey === differenceKeys) {
                   return {
                     key: objKey,
                     index: index
-                  }
+                  };
                 }
-                return []
-              }))
+                return [];
+              }));
             })),
-            inLess: _.flatten(_.difference(otherObj.fileKeys, obj.fileKeys).map(function(differenceKeys){
-              return _.flatten(otherObj.fileKeys.map(function(objKey, index){
-                if(objKey == differenceKeys){
+            inLess: _.flatten(_.difference(otherObj.fileKeys, obj.fileKeys).map(function (differenceKeys) {
+              return _.flatten(otherObj.fileKeys.map(function (objKey, index) {
+                if (objKey === differenceKeys) {
                   return {
                     key: objKey,
                     index: index
-                  }
+                  };
                 }
-                return []
-              }))
+                return [];
+              }));
             }))
-          }
+          };
         }))
-      }
-    })
+      };
+    });
   }
 };
--- tests/jsobject.test.js
+++ tests/jsobject.test.js
@@ -1,29 +1,54 @@
 'use strict';
+
 var jsobjectDiff = require('../');
 var fs = require('fs');
 var t = require('chai').assert;
-var json = JSON.parse(fs.readFileSync('test.json', 'utf8'));
-
+var path = require('path');
+var json = require('./test.json');
 
 describe('jsobject-diff', function () {
   describe('compare(files) with two files', function () {
     it('should return an error if the files are not matching', function (done) {
       var files = json.files.map(function (fileTab) {
+        var absolutePath = path.resolve(__dirname, fileTab.filePath);
         return {
-          filepath: fileTab[0],
-          objectpath: fileTab[1].split('.'),
-          extract: function (object) {
-            function findObject(object, path, i) {
-              if (i < path.length - 1) {
-                return findObject(object[path[i]], path, ++i);
-              }
-              return object[path[i]];
-            }
-            return findObject(object, this.objectpath, 0);
-          }
+          filePath: fileTab.filePath,
+          objectPath: fileTab.objectPath,
+          content: fs.readFileSync(absolutePath, 'utf-8')
         };
       });
-      t.deepEqual(jsobjectDiff.compare(json.pre, files, json.post), ["SEARCH_TEMPLATE_INTRO", "SEARCH_SOURCES_RESERVED_INTRO"]);
+
+      t.deepEqual(jsobjectDiff.compare(json.pre, files, json.post), [{
+        "filePath": "./fixtures/en_US.js",
+        "objectPath": "i18n.en_US",
+        "comparedTo": [{
+          "filePath": "./fixtures/fr_FR.js",
+          "objectPath": "i18n.fr_FR",
+          "inPlus": [{
+            "key": "SEARCH_TEMPLATE_INTRO",
+            "index": 1
+          }],
+          "inLess": [{
+            "key": "SEARCH_SOURCES_RESERVED_INTRO",
+            "index": 1
+          }]
+        }]
+      }, {
+        "filePath": "./fixtures/fr_FR.js",
+        "objectPath": "i18n.fr_FR",
+        "comparedTo": [{
+          "filePath": "./fixtures/en_US.js",
+          "objectPath": "i18n.en_US",
+          "inPlus": [{
+            "key": "SEARCH_SOURCES_RESERVED_INTRO",
+            "index": 1
+          }],
+          "inLess": [{
+            "key": "SEARCH_TEMPLATE_INTRO",
+            "index": 1
+          }]
+        }]
+      }]);
       done();
     });
   });
